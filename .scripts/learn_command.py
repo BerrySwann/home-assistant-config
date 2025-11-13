53474129d0b5a2d36190d788acebb467fa4cc4bc
Eric Rodi (HAOS) <erodi@users.noreply.github.com>
2025-11-13 19:10:03 +0100

    HAOS auto-backup: 2025-11-13 19:10:03 CET (HA 2025.11.1)

--- .scripts/learn_command.py
+++ .scripts/learn_command.py
@@ -5,10 +5,12 @@ headers = {
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2NzE2NjU4YjhhM2U0MGJhYjg5YzNmYWRhZmNjZTdlNSIsImlhdCI6MTc2MzA1Mjc2MywiZXhwIjoyMDc4NDEyNzYzfQ.2xRVc4nU84hcTEqgxVad3MsuZG5TlcvBln8eEE2uVp8",
     "Content-Type": "application/json"
 }
-{
+
+data = {
     "entity_id": "remote.clim_bureau",
-    "command": "cmd1"
+
 }
+
 
 response = requests.post(url, headers=headers, json=data)
 
