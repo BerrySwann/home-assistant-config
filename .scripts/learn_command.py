4bcd05875233b9b1eacf52d1e314d50e2afacc0c
Eric Rodi (HAOS) <erodi@users.noreply.github.com>
2025-11-13 18:10:03 +0100

    HAOS auto-backup: 2025-11-13 18:10:03 CET (HA 2025.11.1)

--- .scripts/learn_command.py
+++ .scripts/learn_command.py
@@ -1,13 +1,13 @@
 import requests
 
-url = "http://homeassistant.local:8123/api/services/remote/learn_command"
+url = "http://192.168.1.96:8123/api/services/remote/learn_command"
 headers = {
-    "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2NzE2NjU4YjhhM2U0MGJhYjg5YzNmYWRhZmNjZTdlNSIsImlhdCI6MTc2MzA1Mjc2MywiZXhwIjoyMDc4NDEyNzYzfQ.2xRVc4nU84hcTEqgxVad3MsuZG5TlcvBln8eEE2uVp8",
-    "Content-Type": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2NzE2NjU4YjhhM2U0MGJhYjg5YzNmYWRhZmNjZTdlNSIsImlhdCI6MTc2MzA1Mjc2MywiZXhwIjoyMDc4NDEyNzYzfQ.2xRVc4nU84hcTEqgxVad3MsuZG5TlcvBln8eEE2uVp8",
+    "Content-Type": "application/json"
 }
-data = {
+{
     "entity_id": "remote.clim_bureau",
-    "command": "heat_auto_17"
+    "command": "cmd1"
 }
 
 response = requests.post(url, headers=headers, json=data)
