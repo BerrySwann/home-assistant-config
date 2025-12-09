ebeb685a1cf2d1be6b102b3a8de1f89499c40e79
Eric Rodi (HAOS) <erodi@users.noreply.github.com>
2025-12-09 19:20:56 +0100

    HAOS auto-backup: 2025-12-09 19:20:56 CET (HA 2025.12.2)

--- python_scripts/gemini_log.py
+++ python_scripts/gemini_log.py
@@ -1,9 +1,11 @@
 # Fichier: python_scripts/gemini_log.py
-# Ce script met à jour l'entité sensor.gemini_export_data en utilisant son attribut 'data'
-# pour stocker la longue chaîne JSON.
 
+# Récupère la charge JSON passée par le script Home Assistant
 data = data.get('json_payload', '{}')
 entity_id = 'sensor.gemini_export_data'
+
+# Crée un attribut 'data_payload' et y stocke la longue chaîne JSON
 attributes = {'data_payload': data}
 
+# Définit l'état du capteur sur "READY" et ajoute l'attribut
 hass.states.set(entity_id, 'READY', attributes)
